c20fb2419c193d50aff679842313ea0dc3af89d1
manhalrahman <mrmanhal99@gmail.com>
2022-07-18 12:31:31 +0530

    changed groupNo to non-null value

--- src/main/kotlin/com/manhalrahman/entities/User.kt
+++ src/main/kotlin/com/manhalrahman/entities/User.kt
@@ -2,7 +2,7 @@ package com.manhalrahman.entities
 
 data class User(
     val userId: Int,
-    val groupNo: Int?,
+    val groupNo: Int,
     val name: String,
     val email: String,
     val phoneNumber: String
